dadde59c9a0faf2a09371ffdf601076cf8b95ec0
Daniel B <1996.daniel.bryant@gmail.com>
2020-01-27 09:15:22 +0100

    week2

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,17 @@
+
+
+def fizz_buzz(number)
+    if (number%3==0) && (number%5==0)
+        'fizzBuzz'
+    elsif  number%3==0
+        
+        'fizz'
+    elsif number%5==0
+        
+        'buzz'
+        
+    else
+    number
+    end
+
+end
